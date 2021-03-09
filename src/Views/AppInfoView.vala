sudo-give-me-coffee/shop

feat: Pop!_OS Patches

src/Views/AppInfoView.vala
@@ -28,6 +28,7 @@ namespace AppCenter.Views {
             Gtk.StackTransitionType transition_type = Gtk.StackTransitionType.SLIDE_LEFT_RIGHT
         );
 
+        private static Gtk.CssProvider arrow_provider;
         private static Gtk.CssProvider loading_provider;
         private static Gtk.CssProvider? previous_css_provider = null;
 
@@ -50,6 +51,8 @@ namespace AppCenter.Views {
         private Widgets.SizeLabel size_label;
         private Widgets.Switcher screenshot_switcher;
 
+        private Gtk.Label app_version;
+
         public bool to_recycle { public get; private set; default = false; }
 
         public AppInfoView (AppCenterCore.Package package) {
@@ -57,6 +60,9 @@ namespace AppCenter.Views {
         }
 
         static construct {
+            arrow_provider = new Gtk.CssProvider ();
+            arrow_provider.load_from_resource ("io/elementary/appcenter/arrow.css");
+
             loading_provider = new Gtk.CssProvider ();
             loading_provider.load_from_resource ("io/elementary/appcenter/loading.css");
         }
@@ -172,7 +178,14 @@ namespace AppCenter.Views {
             package_name.selectable = true;
             package_name.xalign = 0;
             package_name.get_style_context ().add_class (Granite.STYLE_CLASS_H1_LABEL);
-            package_name.valign = Gtk.Align.END;
+
+            app_version = new Gtk.Label (null);
+            app_version.margin_top = 12;
+            app_version.xalign = 0;
+            app_version.hexpand = true;
+            app_version.valign = Gtk.Align.CENTER;
+            app_version.get_style_context ().add_class (Gtk.STYLE_CLASS_DIM_LABEL);
+            app_version.get_style_context ().add_class (Granite.STYLE_CLASS_H3_LABEL);
 
             package_author.selectable = true;
             package_author.xalign = 0;
@@ -280,7 +293,7 @@ namespace AppCenter.Views {
 
             origin_liststore = new Gtk.ListStore (2, typeof (AppCenterCore.Package), typeof (string));
             origin_combo = new Gtk.ComboBox.with_model (origin_liststore);
-            origin_combo.halign = Gtk.Align.START;
+            origin_combo.halign = Gtk.Align.FILL;
             origin_combo.valign = Gtk.Align.START;
             origin_combo.changed.connect (() => {
                 Gtk.TreeIter iter;
@@ -293,6 +306,7 @@ namespace AppCenter.Views {
             });
 
             origin_combo_revealer = new Gtk.Revealer ();
+            origin_combo_revealer.halign = Gtk.Align.FILL;
             origin_combo_revealer.add (origin_combo);
 
             var renderer = new Gtk.CellRendererText ();
@@ -307,33 +321,43 @@ namespace AppCenter.Views {
              * progress grid */
             progress_grid.margin_end = 6;
             progress_grid.margin_top = 12;
-            button_grid.margin_top = progress_grid.margin_top;
 
             var header_grid = new Gtk.Grid ();
             header_grid.column_spacing = 12;
             header_grid.row_spacing = 6;
-            header_grid.hexpand = true;
+            header_grid.halign = Gtk.Align.START;
+            header_grid.valign = Gtk.Align.CENTER;
             header_grid.attach (image, 0, 0, 1, 3);
             header_grid.attach (package_name, 1, 0);
-            header_grid.attach (package_author, 1, 1);
-            header_grid.attach (origin_combo_revealer, 1, 2);
-            header_grid.attach (action_stack, 3, 0);
+            header_grid.attach (package_author, 1, 1, 2);
+            header_grid.attach (app_version, 2, 0);
+
+            button_grid.attach (origin_combo_revealer, 0, 0, 1, 1);
+
+            action_stack.halign = Gtk.Align.FILL;
+            action_stack.valign = Gtk.Align.CENTER;
+
+            var header_flow = new Gtk.FlowBox ();
+            header_flow.min_children_per_line = 1;
+            header_flow.max_children_per_line = 2;
+            header_flow.hexpand = true;
+            header_flow.margin = content_grid.margin / 2;
+            header_flow.add (header_grid);
+            header_flow.add (action_stack);
 
             if (!package.is_local) {
                 size_label = new Widgets.SizeLabel ();
-                size_label.halign = Gtk.Align.END;
-                size_label.valign = Gtk.Align.START;
                 size_label.get_style_context ().add_class (Gtk.STYLE_CLASS_DIM_LABEL);
 
                 action_button_group.add_widget (size_label);
 
-                header_grid.attach (size_label, 3, 1);
+                header_grid.attach (size_label, 1, 2, 2);
             }
 
             var header_column = new Hdy.Column ();
             header_column.margin = 24;
             header_column.maximum_width = MAX_WIDTH;
-            header_column.add (header_grid);
+            header_column.add (header_flow);
 
             var header_box = new Gtk.Grid ();
             header_box.get_style_context ().add_class ("banner");
@@ -434,7 +458,6 @@ namespace AppCenter.Views {
                 var other_apps_grid = new Gtk.Grid ();
                 other_apps_grid.orientation = Gtk.Orientation.VERTICAL;
                 other_apps_grid.row_spacing = 12;
-                other_apps_grid.width_request = MAX_WIDTH;
                 other_apps_grid.add (other_apps_header);
                 other_apps_grid.add (other_apps_carousel);
 
@@ -519,6 +542,10 @@ namespace AppCenter.Views {
         }
 
         protected override void update_state (bool first_update = false) {
+            if (!first_update && !package.is_os_updates) {
+                app_version.label = package.get_version ();
+            }
+
             size_label.update ();
             if (package.state == AppCenterCore.Package.State.NOT_INSTALLED) {
                 get_app_download_size.begin ();
@@ -606,7 +633,7 @@ namespace AppCenter.Views {
                 }
 
                 count++;
-                if (count > 1) {
+                if (count > 0) {
                     origin_combo_revealer.reveal_child = true;
                 }
             }
@@ -719,15 +746,9 @@ namespace AppCenter.Views {
 
         // We need to first download the screenshot locally so that it doesn't freeze the interface.
         private void load_screenshot (string path) {
-            var scale_factor = get_scale_factor ();
             try {
-                var pixbuf = new Gdk.Pixbuf.from_file_at_scale (path, MAX_WIDTH * scale_factor, 600 * scale_factor, true);
-                var image = new Gtk.Image ();
-                image.width_request = MAX_WIDTH;
-                image.height_request = 500;
-                image.icon_name = "image-x-generic";
-                image.halign = Gtk.Align.CENTER;
-                image.gicon = pixbuf;
+                AppCenter.Widgets.AppScreenshot image = new AppCenter.Widgets.AppScreenshot ();
+                image.set_path (path);
 
                 Idle.add (() => {
                     image.show ();

elementary/appcenter

src/Views/AppInfoView.vala
@@ -27,6 +27,7 @@ namespace AppCenter.Views {
             Gtk.StackTransitionType transition_type = Gtk.StackTransitionType.SLIDE_LEFT_RIGHT
         );
 
+        private static Gtk.CssProvider arrow_provider;
         private static Gtk.CssProvider banner_provider;
         private static Gtk.CssProvider loading_provider;
         private static Gtk.CssProvider? previous_css_provider = null;
@@ -51,6 +52,8 @@ namespace AppCenter.Views {
         private ArrowButton screenshot_next;
         private ArrowButton screenshot_previous;
 
+        private Gtk.Label app_version;
+
         public bool to_recycle { public get; private set; default = false; }
 
         public AppInfoView (AppCenterCore.Package package) {
@@ -58,9 +61,10 @@ namespace AppCenter.Views {
         }
 
         static construct {
+            arrow_provider = new Gtk.CssProvider ();
+            arrow_provider.load_from_resource ("io/elementary/appcenter/arrow.css");
             banner_provider = new Gtk.CssProvider ();
             banner_provider.load_from_resource ("io/elementary/appcenter/banner.css");
-
             loading_provider = new Gtk.CssProvider ();
             loading_provider.load_from_resource ("io/elementary/appcenter/loading.css");
         }
@@ -204,7 +208,14 @@ namespace AppCenter.Views {
             package_name.selectable = true;
             package_name.xalign = 0;
             package_name.get_style_context ().add_class (Granite.STYLE_CLASS_H1_LABEL);
-            package_name.valign = Gtk.Align.END;
+
+            app_version = new Gtk.Label (null);
+            app_version.margin_top = 12;
+            app_version.xalign = 0;
+            app_version.hexpand = true;
+            app_version.valign = Gtk.Align.CENTER;
+            app_version.get_style_context ().add_class (Gtk.STYLE_CLASS_DIM_LABEL);
+            app_version.get_style_context ().add_class (Granite.STYLE_CLASS_H3_LABEL);
 
             package_author.selectable = true;
             package_author.xalign = 0;
@@ -312,7 +323,7 @@ namespace AppCenter.Views {
 
             origin_liststore = new Gtk.ListStore (2, typeof (AppCenterCore.Package), typeof (string));
             origin_combo = new Gtk.ComboBox.with_model (origin_liststore);
-            origin_combo.halign = Gtk.Align.START;
+            origin_combo.halign = Gtk.Align.FILL;
             origin_combo.valign = Gtk.Align.START;
             origin_combo.changed.connect (() => {
                 Gtk.TreeIter iter;
@@ -325,6 +336,7 @@ namespace AppCenter.Views {
             });
 
             origin_combo_revealer = new Gtk.Revealer ();
+            origin_combo_revealer.halign = Gtk.Align.FILL;
             origin_combo_revealer.add (origin_combo);
 
             var renderer = new Gtk.CellRendererText ();
@@ -339,34 +351,44 @@ namespace AppCenter.Views {
              * progress grid */
             progress_grid.margin_end = 6;
             progress_grid.margin_top = 12;
-            button_grid.margin_top = progress_grid.margin_top;
 
             var header_grid = new Gtk.Grid ();
             header_grid.column_spacing = 12;
             header_grid.row_spacing = 6;
-            header_grid.hexpand = true;
+            header_grid.halign = Gtk.Align.START;
+            header_grid.valign = Gtk.Align.CENTER;
             header_grid.attach (image, 0, 0, 1, 3);
             header_grid.attach (package_name, 1, 0);
-            header_grid.attach (package_author, 1, 1);
-            header_grid.attach (origin_combo_revealer, 1, 2, 3);
-            header_grid.attach (action_stack, 3, 0);
+            header_grid.attach (package_author, 1, 1, 2);
+            header_grid.attach (app_version, 2, 0);
+
+            button_grid.attach (origin_combo_revealer, 0, 0, 1, 1);
+
+            action_stack.halign = Gtk.Align.FILL;
+            action_stack.valign = Gtk.Align.CENTER;
+
+            var header_flow = new Gtk.FlowBox ();
+            header_flow.min_children_per_line = 1;
+            header_flow.max_children_per_line = 2;
+            header_flow.hexpand = true;
+            header_flow.margin = content_grid.margin / 2;
+            header_flow.add (header_grid);
+            header_flow.add (action_stack);
 
             if (!package.is_local) {
                 size_label = new Widgets.SizeLabel ();
-                size_label.halign = Gtk.Align.END;
-                size_label.valign = Gtk.Align.START;
                 size_label.get_style_context ().add_class (Gtk.STYLE_CLASS_DIM_LABEL);
 
                 action_button_group.add_widget (size_label);
 
-                header_grid.attach (size_label, 3, 1);
+                header_grid.attach (size_label, 1, 2, 2);
             }
 
             var header_clamp = new Hdy.Clamp () {
                 margin = 24,
                 maximum_size = MAX_WIDTH
             };
-            header_clamp.add (header_grid);
+            header_clamp.add (header_flow);
 
             var header_box = new Gtk.Grid ();
             header_box.get_style_context ().add_class ("banner");
@@ -475,7 +497,6 @@ namespace AppCenter.Views {
                 var other_apps_grid = new Gtk.Grid ();
                 other_apps_grid.orientation = Gtk.Orientation.VERTICAL;
                 other_apps_grid.row_spacing = 12;
-                other_apps_grid.width_request = MAX_WIDTH;
                 other_apps_grid.add (other_apps_header);
                 other_apps_grid.add (other_apps_carousel);
 
@@ -561,6 +582,10 @@ namespace AppCenter.Views {
         }
 
         protected override void update_state (bool first_update = false) {
+            if (!first_update && !package.is_os_updates) {
+                app_version.label = package.get_version ();
+            }
+
             size_label.update ();
             if (package.state == AppCenterCore.Package.State.NOT_INSTALLED) {
                 get_app_download_size.begin ();
@@ -648,7 +673,7 @@ namespace AppCenter.Views {
                 }
 
                 count++;
-                if (count > 1) {
+                if (count > 0) {
                     origin_combo_revealer.reveal_child = true;
                 }
             }
@@ -762,15 +787,9 @@ namespace AppCenter.Views {
 
         // We need to first download the screenshot locally so that it doesn't freeze the interface.
         private void load_screenshot (string path) {
-            var scale_factor = get_scale_factor ();
             try {
-                var pixbuf = new Gdk.Pixbuf.from_file_at_scale (path, MAX_WIDTH * scale_factor, 600 * scale_factor, true);
-                var image = new Gtk.Image ();
-                image.width_request = MAX_WIDTH;
-                image.height_request = 500;
-                image.icon_name = "image-x-generic";
-                image.halign = Gtk.Align.CENTER;
-                image.gicon = pixbuf;
+                AppCenter.Widgets.AppScreenshot image = new AppCenter.Widgets.AppScreenshot ();
+                image.set_path (path);
 
                 Idle.add (() => {
                     image.show ();